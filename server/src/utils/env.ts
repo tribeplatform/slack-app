7e5ae7213586e30407583ca1f423765b4ec2b004
Amir Ahangari <amir@ahangari.org>
2022-08-19 17:12:52 -0400

    fix env validations

--- server/src/utils/env.ts
+++ server/src/utils/env.ts
@@ -1,17 +1,20 @@
-import { cleanEnv, port, str } from 'envalid';
+import { cleanEnv, makeValidator, port, str, url } from 'envalid';
 import { config } from 'dotenv';
-
+const notEmpty = makeValidator((input: string) => {
+  if(!!input && input.length) return input
+  throw new Error('Must not be empty')
+});
 config({ path: `.env` });
 cleanEnv(process.env, {
   NODE_ENV: str(),
   PORT: port(),
   DB_HOST: str(),
-  GRAPHQL_URL: str(),
-  SERVER_URL: str(),
-  CLIENT_ID: str(),
-  CLIENT_SECRET: str(),
-  SIGNING_SECRET: str(),
-  SLACK_CLIENT_ID: str(),
-  SLACK_CLIENT_SECRET: str(),
-  LOG_FORMAT: str({ choices: ['info', 'debug'] }),
+  GRAPHQL_URL: url(),
+  SERVER_URL: url(),
+  CLIENT_ID: notEmpty(),
+  CLIENT_SECRET: notEmpty(),
+  SIGNING_SECRET: notEmpty(),
+  SLACK_CLIENT_ID: notEmpty(),
+  SLACK_CLIENT_SECRET: notEmpty(),
+  LOG_FORMAT: str({ devDefault: 'debug', default: 'info', choices: ['info', 'debug'] }),
 });
