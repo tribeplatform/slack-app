6d2f432e941b1251b6507e76b2425a1e25d7455f
Amir Ahangari <amir@ahangari.org>
2022-08-19 17:03:56 -0400

    refactor env file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,5 @@
-import { init } from '@/utils/env';
-init()
-import { logger } from '@utils/logger'
+import '@/utils/env';
+import '@utils/tracer'
 import App from '@/app';
 import HealthRoute from '@routes/health.route';
 import WebhookRoute from '@routes/webhook.route';
--- server/src/utils/env.ts
+++ server/src/utils/env.ts
@@ -1,23 +1,17 @@
 import { cleanEnv, port, str } from 'envalid';
 import { config } from 'dotenv';
+
 config({ path: `.env` });
-
-const validateEnv = () => {
-  cleanEnv(process.env, {
-    NODE_ENV: str(),
-    PORT: port(),
-    DB_HOST: str(),
-    GRAPHQL_URL: str(),
-    SERVER_URL: str(),
-    CLIENT_ID: str(),
-    CLIENT_SECRET: str(),
-    SIGNING_SECRET: str(),
-    SLACK_CLIENT_ID: str(),
-    SLACK_CLIENT_SECRET: str(),
-  });
-};
-
-export const init = () => {
-  config({ path: `.env` });
-  validateEnv()
-};
+cleanEnv(process.env, {
+  NODE_ENV: str(),
+  PORT: port(),
+  DB_HOST: str(),
+  GRAPHQL_URL: str(),
+  SERVER_URL: str(),
+  CLIENT_ID: str(),
+  CLIENT_SECRET: str(),
+  SIGNING_SECRET: str(),
+  SLACK_CLIENT_ID: str(),
+  SLACK_CLIENT_SECRET: str(),
+  LOG_FORMAT: str({ choices: ['info', 'debug'] }),
+});
